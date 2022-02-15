d72f41bd62b4a4a331c751aa83bd1af9b53db403
Gabriel Augusto <86443677+bielsantanape@users.noreply.github.com>
2022-02-15 10:24:48 -0300

    Update Q3 Teste 2 FAC.c

--- Q3 Teste 2 FAC.c
+++ Q3 Teste 2 FAC.c
@@ -28,13 +28,14 @@ void zera (float v[], int n){
     v[i]=0;
   }
 }
+
 int busca(int v[], int n, int chv){
-	int i;
-	for (i=0;i<n;i++){
-    if( v[i]==chv) 
-      return i;
-	}
-	return -1; 
+  int i;
+  for (i=0;i<n;i++){
+     if( v[i]==chv) 
+       return i;
+  }
+  return -1; 
 }
 
 void fContabiliza(int vMatrAl[],int vCursoAl[],int l,int vCod[],int vUnid[],float vValor[],int k, float vTotUnid[]){
@@ -51,16 +52,13 @@ void fContabiliza(int vMatrAl[],int vCursoAl[],int l,int vCod[],int vUnid[],floa
     if (posunidade<UNID){
       vTotUnid[posunidade]+=vValor[pos];
     }
-
   }
-
 }
 
 int main(void) {
   int vCod[CUR]= {23,12,77,45,90,78,56,43,11,912,9,22};
   int vUnid[CUR]= {1,8,6,2,3,9,4,5,6,8,7,1};
   float vValor[CUR]={100,200,150,400,500,350,250,100,900,500,250,650};
- 
   int vMatrAl[AL]= {141,79,159,110,115,138,54,200,163,77,137,105,199,124,166,127,50,135,189,159,120,64,125,139,75,178,84,176,133,116,71,182,168,129,104};
   int vCursoAl[AL]={23,12,77,45,90,78,56,43,11,912,9,22,23,12,77,45,90,78,56,43,11,912,9,22,912,9,22,23,12,77,45,90,78,56,43};
   
@@ -68,9 +66,6 @@ int main(void) {
   int ii;
   int i;
 
-  //zera(vContUnid);
-
-  //funcAleatoria (vMatrAl,vCursoAl,vCod,vUnid,vContUnid,vValor);
   zera(vTotUnid,UNID);
   printf("\n************");
   fContabiliza (vMatrAl,vCursoAl,AL,vCod,vUnid,vValor,CUR,vTotUnid);
@@ -78,7 +73,6 @@ int main(void) {
   for (i=0;i<8;i++){
     printf("\nUnid: %d - Total: %.2f",i+1,vTotUnid[i]);
   }
-  
-  
+
   return 0;
 }
