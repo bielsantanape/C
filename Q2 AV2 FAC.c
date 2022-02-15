6584256a998a2dc566380ce6e0a06c0d4576bed7
Gabriel Augusto <86443677+bielsantanape@users.noreply.github.com>
2022-02-15 10:06:41 -0300

    Update Q2 AV2 FAC.c

--- Q2 AV2 FAC.c
+++ Q2 AV2 FAC.c
@@ -1,3 +1,12 @@
+/*
+a) Construa uma função que receba dois vetores de valores inteiros A e B de tamanho N e mostre as posições
+onde a diferença absoluta entre A e B é par, bem como os valores de A, B e a diferença absoluta.
+Exemplo:
+A={3,7,6,5,2} B={9,2,5,1,3} ---&gt; posição 0: A:3 B:9 diferença absoluta:6
+posição 3: A:5 B:1 diferença absoluta:4
+b) Construa uma função main para testar sua função, Crie os vetores por enumeração.*/
+
+
 #include <stdio.h>
  
 void mostrarPosicaoAbsoluta(int v1[],int v2[], int tam){
